7c4c7fea16eb265b334edcc578d98575f697fd61
Ivan Anugerah Kautsar <ivan.anugerah.k@gmail.com>
2025-07-25 13:46:07 +0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,15 +5,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const mobileMenu = document.getElementById('mobileMenu');
   const overlay = document.getElementById('overlay');
   const closeButton = document.getElementById('closeButton');
+  const body = document.body;
 
   function openMenu() {
     mobileMenu.classList.remove('translate-x-full');
     overlay.classList.remove('hidden');
+    body.classList.add('overflow-hidden'); // mencegah scroll
   }
 
   function closeMenu() {
     mobileMenu.classList.add('translate-x-full');
     overlay.classList.add('hidden');
+    body.classList.remove('overflow-hidden'); // izinkan scroll kembali
   }
 
   menuButton.addEventListener('click', openMenu);
